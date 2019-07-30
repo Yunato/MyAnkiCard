f844ded3f332bb2b89cfb7ddcc103c1bab551b35
Yunato <yunato525@gmail.com>
2019-07-31 07:24:58 +0900

    Connect MainFragment to MainActivity (Add dummy_content layout)

--- app/src/main/java/io/github/yunato/myankicard/ui/activity/MainActivity.kt
+++ app/src/main/java/io/github/yunato/myankicard/ui/activity/MainActivity.kt
@@ -3,11 +3,15 @@ package io.github.yunato.myankicard.ui.activity
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import io.github.yunato.myankicard.R
+import io.github.yunato.myankicard.ui.fragment.MainFragment
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val fragment = MainFragment.newInstance()
+        supportFragmentManager.beginTransaction().replace(R.id.content, fragment).commit()
     }
 }
