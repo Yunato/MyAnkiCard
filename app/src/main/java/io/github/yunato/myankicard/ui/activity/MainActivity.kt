3eee83d32cd3aacab8aba509252b90078553722b
Yunato <yunato525@gmail.com>
2019-07-31 23:59:39 +0900

    Add getting timestamp for saving to SharedPreferences to check first run on day

--- app/src/main/java/io/github/yunato/myankicard/ui/activity/MainActivity.kt
+++ app/src/main/java/io/github/yunato/myankicard/ui/activity/MainActivity.kt
@@ -1,9 +1,12 @@
 package io.github.yunato.myankicard.ui.activity
 
+import android.content.SharedPreferences
 import android.os.Bundle
+import android.preference.PreferenceManager
 import android.support.v7.app.AppCompatActivity
 import io.github.yunato.myankicard.R
 import io.github.yunato.myankicard.ui.fragment.MainFragment
+import java.util.*
 
 class MainActivity : AppCompatActivity() {
 
@@ -18,5 +21,35 @@ class MainActivity : AppCompatActivity() {
             fragment = MainFragment.newInstance()
             fm.beginTransaction().replace(R.id.fragment_container, fragment).commit()
         }
+    }
+
+    override fun onResume() {
+        super.onResume()
+
+        val now = Calendar.getInstance()
+        now.set(Calendar.HOUR, 0)
+        now.set(Calendar.MINUTE, 0)
+        now.set(Calendar.SECOND, 0)
+        now.set(Calendar.MILLISECOND, 0)
+        val stamp = now.time.time / 1000
+        if(getStamp() != stamp){
+            // TODO DB check
+            setStamp(stamp)
+        }
+    }
+
+    private fun setStamp(stamp: Long) {
+        val sp: SharedPreferences = PreferenceManager.getDefaultSharedPreferences(this)
+        sp.edit().putLong(STATE_STAMP, stamp).apply()
+    }
+
+    private fun getStamp(): Long {
+        val sp: SharedPreferences = PreferenceManager.getDefaultSharedPreferences(this)
+        return sp.getLong(STATE_STAMP, PREFERENCE_INIT)
+    }
+
+    companion object {
+        @JvmStatic private val STATE_STAMP: String = "io.github.yunato.myankicard.ui.activity.STATE_STAMP"
+        @JvmStatic private val PREFERENCE_INIT: Long = 0
     }
 }
