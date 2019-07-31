0342f414057a56d36821699e66212c050c3b9ba5
Yunato <yunato525@gmail.com>
2019-07-31 23:13:13 +0900

    Add NonSwipeableViewPager (Custom ViewPager)

---
+++ app/src/main/java/io/github/yunato/myankicard/ui/view/NonSwipeableViewPager.kt
@@ -0,0 +1,17 @@
+package io.github.yunato.myankicard.ui.view
+
+import android.content.Context
+import android.support.v4.view.ViewPager
+import android.util.AttributeSet
+import android.view.MotionEvent
+
+class NonSwipeableViewPager: ViewPager {
+
+    constructor(context: Context): super(context)
+
+    constructor(context: Context, attrs: AttributeSet?): super(context, attrs)
+
+    override fun onInterceptTouchEvent(ev: MotionEvent?): Boolean = false
+
+    override fun onTouchEvent(ev: MotionEvent?): Boolean = false
+}
