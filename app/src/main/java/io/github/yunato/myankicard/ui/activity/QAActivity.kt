25b435f107f392e70673b309e1caed702183b86f
Yunato <yunato525@gmail.com>
2019-07-31 15:58:29 +0900

    Connect QAFragment to QAActivity

--- app/src/main/java/io/github/yunato/myankicard/ui/activity/QAActivity.kt
+++ app/src/main/java/io/github/yunato/myankicard/ui/activity/QAActivity.kt
@@ -3,11 +3,20 @@ package io.github.yunato.myankicard.ui.activity
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import io.github.yunato.myankicard.R
+import io.github.yunato.myankicard.ui.fragment.QAFragment
 
 class QAActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_qa)
+
+        val fm = supportFragmentManager
+        var fragment = fm.findFragmentById(R.id.fragment_container)
+
+        if (fragment == null){
+            fragment = QAFragment.newInstance()
+            fm.beginTransaction().replace(R.id.fragment_container, fragment).commit()
+        }
     }
 }
