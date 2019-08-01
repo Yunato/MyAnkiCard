c0402aa9443b28a2db5dda451d63180a8d1570ec
Yunato <yunato525@gmail.com>
2019-08-01 14:57:38 +0900

    Move func checking time stamp from MainActivity to InitialActivity

--- app/src/main/java/io/github/yunato/myankicard/ui/activity/InitialActivity.kt
+++ app/src/main/java/io/github/yunato/myankicard/ui/activity/InitialActivity.kt
@@ -1,10 +1,13 @@
 package io.github.yunato.myankicard.ui.activity
 
+import android.content.SharedPreferences
 import android.os.Bundle
+import android.preference.PreferenceManager
 import android.support.v7.app.AppCompatActivity
 import io.github.yunato.myankicard.R
 import io.github.yunato.myankicard.model.entity.AnkiCard
 import io.github.yunato.myankicard.other.aws.DailyCardsTask
+import java.util.*
 
 class InitialActivity : AppCompatActivity() {
 
@@ -19,5 +22,35 @@ class InitialActivity : AppCompatActivity() {
             }
         })
         getTask.execute()
+    }
+
+    override fun onResume() {
+        super.onResume()
+
+        val now = Calendar.getInstance()
+        now.set(Calendar.HOUR, 0)
+        now.set(Calendar.MINUTE, 0)
+        now.set(Calendar.SECOND, 0)
+        now.set(Calendar.MILLISECOND, 0)
+        val stamp = now.time.time / 1000
+        if(getStamp() != stamp){
+            // TODO DB check
+            setStamp(stamp)
+        }
+    }
+
+    private fun setStamp(stamp: Long) {
+        val sp: SharedPreferences = PreferenceManager.getDefaultSharedPreferences(this)
+        sp.edit().putLong(STATE_STAMP, stamp).apply()
+    }
+
+    private fun getStamp(): Long {
+        val sp: SharedPreferences = PreferenceManager.getDefaultSharedPreferences(this)
+        return sp.getLong(STATE_STAMP, PREFERENCE_INIT)
+    }
+
+    companion object {
+        @JvmStatic private val STATE_STAMP: String = "io.github.yunato.myankicard.ui.activity.STATE_STAMP"
+        @JvmStatic private val PREFERENCE_INIT: Long = 0
     }
 }
--- app/src/main/java/io/github/yunato/myankicard/ui/activity/MainActivity.kt
+++ app/src/main/java/io/github/yunato/myankicard/ui/activity/MainActivity.kt
@@ -2,13 +2,10 @@ package io.github.yunato.myankicard.ui.activity
 
 import android.content.Context
 import android.content.Intent
-import android.content.SharedPreferences
 import android.os.Bundle
-import android.preference.PreferenceManager
 import android.support.v7.app.AppCompatActivity
 import io.github.yunato.myankicard.R
 import io.github.yunato.myankicard.ui.fragment.MainFragment
-import java.util.*
 
 class MainActivity : AppCompatActivity() {
 
@@ -25,35 +22,7 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
-    override fun onResume() {
-        super.onResume()
-
-        val now = Calendar.getInstance()
-        now.set(Calendar.HOUR, 0)
-        now.set(Calendar.MINUTE, 0)
-        now.set(Calendar.SECOND, 0)
-        now.set(Calendar.MILLISECOND, 0)
-        val stamp = now.time.time / 1000
-        if(getStamp() != stamp){
-            // TODO DB check
-            setStamp(stamp)
-        }
-    }
-
-    private fun setStamp(stamp: Long) {
-        val sp: SharedPreferences = PreferenceManager.getDefaultSharedPreferences(this)
-        sp.edit().putLong(STATE_STAMP, stamp).apply()
-    }
-
-    private fun getStamp(): Long {
-        val sp: SharedPreferences = PreferenceManager.getDefaultSharedPreferences(this)
-        return sp.getLong(STATE_STAMP, PREFERENCE_INIT)
-    }
-
     companion object {
-        @JvmStatic private val STATE_STAMP: String = "io.github.yunato.myankicard.ui.activity.STATE_STAMP"
-        @JvmStatic private val PREFERENCE_INIT: Long = 0
-
         fun intent(context: Context): Intent =
             Intent(context, MainActivity::class.java).apply {
                 this.addFlags(Intent.FLAG_ACTIVITY_NO_ANIMATION)
