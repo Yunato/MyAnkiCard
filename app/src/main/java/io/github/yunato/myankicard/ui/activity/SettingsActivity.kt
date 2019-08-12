ad80e8d73611b4f2403c2f983de4f11c914e9ab6
Yunato <yunato525@gmail.com>
2019-08-12 10:47:27 +0900

    Add SettingsActivity

---
+++ app/src/main/java/io/github/yunato/myankicard/ui/activity/SettingsActivity.kt
@@ -0,0 +1,25 @@
+package io.github.yunato.myankicard.ui.activity
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.preference.PreferenceFragmentCompat
+import io.github.yunato.myankicard.R
+
+class SettingsActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.settings_activity)
+        supportFragmentManager
+            .beginTransaction()
+            .replace(R.id.settings, SettingsFragment())
+            .commit()
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+    }
+
+    class SettingsFragment : PreferenceFragmentCompat() {
+        override fun onCreatePreferences(savedInstanceState: Bundle?, rootKey: String?) {
+            setPreferencesFromResource(R.xml.root_preferences, rootKey)
+        }
+    }
+}
