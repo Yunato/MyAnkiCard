085284bdbc217bdd0cefbf6a77f6b80923fe22ae
Yunato <yunato525@gmail.com>
2019-08-14 09:44:54 +0900

    Add option menu at MainActivity

--- app/src/main/java/io/github/yunato/myankicard/ui/activity/MainActivity.kt
+++ app/src/main/java/io/github/yunato/myankicard/ui/activity/MainActivity.kt
@@ -3,6 +3,8 @@ package io.github.yunato.myankicard.ui.activity
 import android.content.Context
 import android.content.Intent
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
 import androidx.appcompat.app.AppCompatActivity
 import io.github.yunato.myankicard.R
 import io.github.yunato.myankicard.ui.fragment.MainFragment
@@ -20,6 +22,18 @@ class MainActivity : AppCompatActivity() {
             fragment = MainFragment.newInstance()
             fm.beginTransaction().replace(R.id.fragment_container, fragment).commit()
         }
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.option, menu)
+        return super.onCreateOptionsMenu(menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem?): Boolean {
+        if (item?.itemId == R.id.settings) {
+            startActivity(SettingsActivity.intent(this))
+        }
+        return super.onOptionsItemSelected(item)
     }
 
     companion object {
--- app/src/main/java/io/github/yunato/myankicard/ui/activity/SettingsActivity.kt
+++ app/src/main/java/io/github/yunato/myankicard/ui/activity/SettingsActivity.kt
@@ -3,6 +3,7 @@ package io.github.yunato.myankicard.ui.activity
 import android.content.Context
 import android.content.Intent
 import android.os.Bundle
+import android.view.MenuItem
 import androidx.appcompat.app.AppCompatActivity
 import androidx.preference.PreferenceFragmentCompat
 import io.github.yunato.myankicard.R
@@ -17,6 +18,14 @@ class SettingsActivity : AppCompatActivity() {
             .replace(R.id.settings, SettingsFragment())
             .commit()
         supportActionBar?.setDisplayHomeAsUpEnabled(true)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem?): Boolean {
+        if (item?.itemId == android.R.id.home) {
+            super.onBackPressed()
+            return true
+        }
+        return super.onOptionsItemSelected(item)
     }
 
     class SettingsFragment : PreferenceFragmentCompat() {
